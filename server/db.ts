3a6da37ac97242493f4f4f0223066c20d7885f03
Krishna Niwash <Krishnanivash468@gmail.com>
2026-02-20 22:05:12 +0530

    Added SSL and fixed db connection

--- server/db.ts
+++ server/db.ts
@@ -4,11 +4,14 @@ const { Pool } = pkg;
 import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL must be set in your .env file");
+  throw new Error("DATABASE_URL must be set in your Vercel Environment Variables");
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Neon Database के लिए यह ज़रूरी है
+  }
 });
 
 export const db = drizzle(pool, { schema });
